2819abacf6cb1465bc768c79229eb8f3d7d80eae
matin bahadoran <83409819+matinbhdrn77@users.noreply.github.com>
2022-01-30 04:20:47 -0800

    Delete flights directory

--- flights/admin.py
+++
@@ -1,7 +0,0 @@
-from flights.models import Flight, Airport, Passenger
-from django.contrib import admin
-
-# Register your models here.
-admin.site.register(Airport)
-admin.site.register(Flight)
-admin.site.register(Passenger)
